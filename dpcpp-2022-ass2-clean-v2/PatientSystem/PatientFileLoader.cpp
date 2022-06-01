d866286cfedd78c49f985a2fe78212a254cab162
xutl <418308630@qq.com>
2022-06-01 22:08:32 +0800

    support FR1
    load patients from file

--- dpcpp-2022-ass2-clean-v2/PatientSystem/PatientFileLoader.cpp
+++ dpcpp-2022-ass2-clean-v2/PatientSystem/PatientFileLoader.cpp
@@ -5,21 +5,25 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <algorithm>
 
 #include "Patient.h"
-#include "Vitals.h"
+#include "Vitals.h" 
 
 using namespace std;
-
 
 std::vector<Patient*> PatientFileLoader::loadPatientFile(const std::string& file)
 {
 	vector<Patient*> patients{};
-
+    
     std::ifstream inFile(file);
     if (inFile.is_open()) {
         // TODO: load your file here
+        while (inFile.peek() != -1) {
+            Patient* patient = new Patient{ }; //instantiate a new Patient 
+            inFile >> *patient; // load the patient from the stream
+            patients.push_back(patient); // add the patient to the vector
+        }
     }
-
     return patients;
 }
